4928ed6540ea181f461324a41c6a56cd1cce600f
rukivbruki <0994486@gmail.com>
2020-01-18 23:01:34 +0300

    - Добавляет кнопку возврата на главную страницу; - Добавляет модули.

--- js/index.js
+++ js/index.js
@@ -1,136 +1,8 @@
+import {data, data2, d1, d2, d3, d4} from '../data/data.js';
+
 anychart.onDocumentReady (function () {
 
-    // create a data set
-    let values = ["Калькулятор", "Заявки", "Запрос КП", "Звонки"];
-    let [d1, d2, d3, d4] = values;
-
-    // create data
-    let data = [
-        {month: "2017", [d1]: 120, [d2]: 132, [d3]: 22, [d4]: 960},
-        {month: "2018", [d1]: 143, [d2]: 148, [d3]: 16, [d4]: 981},
-        {month: "2019", [d1]: 153, [d2]: 141, [d3]: 24, [d4]: 1139},
-    ];
-
-    let data2 = [
-        // 2017 год
-        {
-            // Калькулятор
-            [d1]:
-                [{month: "Январь", [d1]: 6}, {month: "Февраль", [d1]: 5},
-                    {month: "Март", [d1]: 3}, {month: "Апрель", [d1]: 0},
-                    {month: "Май", [d1]: 37}, {month: "Июнь", [d1]: 24},
-                    {month: "Июль", [d1]: 13}, {month: "Август", [d1]: 16},
-                    {month: "Сентябрь", [d1]: 6}, {month: "Октябрь", [d1]: 6},
-                    {month: "Ноябрь", [d1]: 4}, {month: "Декабрь", [d1]: 0}
-                ],
-            // Заявки
-            [d2]:
-                [{month: "Январь", [d2]: 10}, {month: "Февраль", [d2]: 12},
-                    {month: "Март", [d2]: 1}, {month: "Апрель", [d2]: 0},
-                    {month: "Май", [d2]: 23}, {month: "Июнь", [d2]: 18},
-                    {month: "Июль", [d2]: 20}, {month: "Август", [d2]: 26},
-                    {month: "Сентябрь", [d2]: 5}, {month: "Октябрь", [d2]: 12},
-                    {month: "Ноябрь", [d2]: 3}, {month: "Декабрь", [d2]: 2}
-                ],
-            // Запрос КП
-            [d3]:
-                [{month: "Январь", [d3]: 1}, {month: "Февраль", [d3]: 2},
-                    {month: "Март", [d3]: 2}, {month: "Апрель", [d3]: 6},
-                    {month: "Май", [d3]: 2}, {month: "Июнь", [d3]: 0},
-                    {month: "Июль", [d3]: 4}, {month: "Август", [d3]: 2},
-                    {month: "Сентябрь", [d3]: 2}, {month: "Октябрь", [d3]: 1},
-                    {month: "Ноябрь", [d3]: 0}, {0: "Декабрь", [d3]: 0},
-                ],
-            // Звонки
-            [d4]:
-                [{month: "Январь", [d4]: 25}, {month: "Февраль", [d4]: 34},
-                    {month: "Март", [d4]: 58}, {month: "Апрель", [d4]: 133},
-                    {month: "Май", [d4]: 206}, {month: "Июнь", [d4]: 148},
-                    {month: "Июль", [d4]: 133}, {month: "Август", [d4]: 73},
-                    {month: "Сентябрь", [d4]: 79}, {month: "Октябрь", [d4]: 55},
-                    {month: "Ноябрь", [d4]: 8}, {0: "Декабрь", [d4]: 8},
-                ]
-        },
-        // 2018 год
-        {
-            // Калькулятор
-            [d1]:
-                [{month: "Январь", [d1]: 11}, {month: "Февраль", [d1]: 2},
-                    {month: "Март", [d1]: 14}, {month: "Апрель", [d1]: 19},
-                    {month: "Май", [d1]: 40}, {month: "Июнь", [d1]: 21},
-                    {month: "Июль", [d1]: 16}, {month: "Август", [d1]: 14},
-                    {month: "Сентябрь", [d1]: 6}, {month: "Октябрь", [d1]: 0},
-                    {month: "Ноябрь", [d1]: 0}, {month: "Декабрь", [d1]: 0}
-                ],
-            // Заявки
-            [d2]:
-                [{month: "Январь", [d2]: 4}, {month: "Февраль", [d2]: 1},
-                    {month: "Март", [d2]: 13}, {month: "Апрель", [d2]: 16},
-                    {month: "Май", [d2]: 37}, {month: "Июнь", [d2]: 25},
-                    {month: "Июль", [d2]: 19}, {month: "Август", [d2]: 20},
-                    {month: "Сентябрь", [d2]: 12}, {month: "Октябрь", [d2]: 1},
-                    {month: "Ноябрь", [d2]: 0}, {month: "Декабрь", [d2]: 0}
-                ],
-            // Запрос КП
-            [d3]:
-                [{month: "Январь", [d3]: 0}, {month: "Февраль", [d3]: 3},
-                    {month: "Март", [d3]: 1}, {month: "Апрель", [d3]: 3},
-                    {month: "Май", [d3]: 3}, {month: "Июнь", [d3]: 2},
-                    {month: "Июль", [d3]: 1}, {month: "Август", [d3]: 2},
-                    {month: "Сентябрь", [d3]: 1}, {month: "Октябрь", [d3]: 0},
-                    {month: "Ноябрь", [d3]: 0}, {0: "Декабрь", [d3]: 0},
-                ],
-            // Звонки
-            [d4]:
-                [{month: "Январь", [d4]: 30}, {month: "Февраль", [d4]: 30},
-                    {month: "Март", [d4]: 60}, {month: "Апрель", [d4]: 138},
-                    {month: "Май", [d4]: 229}, {month: "Июнь", [d4]: 169},
-                    {month: "Июль", [d4]: 132}, {month: "Август", [d4]: 57},
-                    {month: "Сентябрь", [d4]: 83}, {month: "Октябрь", [d4]: 50},
-                    {month: "Ноябрь", [d4]: 0}, {0: "Декабрь", [d4]: 3},
-                ]
-        },
-        // 2019 год
-        {
-            // Калькулятор
-            [d1]:
-                [{month: "Январь", [d1]: 6}, {month: "Февраль", [d1]: 6},
-                    {month: "Март", [d1]: 11}, {month: "Апрель", [d1]: 25},
-                    {month: "Май", [d1]: 29}, {month: "Июнь", [d1]: 13},
-                    {month: "Июль", [d1]: 18}, {month: "Август", [d1]: 16},
-                    {month: "Сентябрь", [d1]: 15}, {month: "Октябрь", [d1]: 9},
-                    {month: "Ноябрь", [d1]: 2}, {month: "Декабрь", [d1]: 3}
-                ],
-            // Заявки
-            [d2]:
-                [{month: "Январь", [d2]: 5}, {month: "Февраль", [d2]: 8},
-                    {month: "Март", [d2]: 10}, {month: "Апрель", [d2]: 24},
-                    {month: "Май", [d2]: 38}, {month: "Июнь", [d2]: 13},
-                    {month: "Июль", [d2]: 10}, {month: "Август", [d2]: 17},
-                    {month: "Сентябрь", [d2]: 6}, {month: "Октябрь", [d2]: 5},
-                    {month: "Ноябрь", [d2]: 4}, {month: "Декабрь", [d2]: 1}
-                ],
-            // Запрос КП
-            [d3]:
-                [{month: "Январь", [d3]: 2}, {month: "Февраль", [d3]: 0},
-                    {month: "Март", [d3]: 0}, {month: "Апрель", [d3]: 2},
-                    {month: "Май", [d3]: 8}, {month: "Июнь", [d3]: 1},
-                    {month: "Июль", [d3]: 5}, {month: "Август", [d3]: 2},
-                    {month: "Сентябрь", [d3]: 2}, {month: "Октябрь", [d3]: 2},
-                    {month: "Ноябрь", [d3]: 0}, {0: "Декабрь", [d3]: 0},
-                ],
-            // Звонки
-            [d4]:
-                [{month: "Январь", [d4]: 21}, {month: "Февраль", [d4]: 53},
-                    {month: "Март", [d4]: 67}, {month: "Апрель", [d4]: 152},
-                    {month: "Май", [d4]: 202}, {month: "Июнь", [d4]: 133},
-                    {month: "Июль", [d4]: 168}, {month: "Август", [d4]: 127},
-                    {month: "Сентябрь", [d4]: 87}, {month: "Октябрь", [d4]: 78},
-                    {month: "Ноябрь", [d4]: 30}, {0: "Декабрь", [d4]: 21},
-                ]
-        }
-    ];
-
+    // Подготовка данных для графиков
     let dataSet = anychart.data.set (data);
 
     // map the data
@@ -157,7 +29,9 @@ anychart.onDocumentReady (function () {
     series4.name (d4);
 
     // configure tooltips
-    chart.tooltip ().format ("{%seriesName} {%value} ({%yPercentOfCategory}{decimalsCount:2}%)");
+    let mainTooltipConf = () => chart.tooltip ()
+        .format ("{%seriesName} {%value} ({%yPercentOfCategory}{decimalsCount:2}%)");
+    mainTooltipConf ();
 
     // configure labels on the y-axis
     chart.yAxis ().labels ().format ("{%value}");
@@ -172,21 +46,40 @@ anychart.onDocumentReady (function () {
     const getKeyByValue = (object, value) => Object.keys (object).find (key => object[key] === value);
 
     // configire drilldown
-    chart.listen ('pointClick', function (e) {
+    let configDrillDown = (e) => {
+        let tmplButton = document.getElementById ('tmpl').content.cloneNode (true);
         let message = e.iterator.getIndex ();
         let value = e.iterator.get ('value');
         let key = getKeyByValue (data[message], value);
 
-        dataSet.data (data2[message][key]);
-        // // set the chart title
+        document.body.append (tmplButton);
+        let button = document.querySelector ('button');
 
+        dataSet.data (data2[message][key]);
+
+        // set the chart title
         chart.title (`Статистика раздела «${key}» 
         за ${data[message]['month']} год`);
 
         // configure tooltips
         chart.tooltip ().format ("{%seriesName} {%value}");
+
+        // возврат на главный экран, удаление кнопки
+        button.addEventListener ('click', (e) => {
+
+            dataSet.data (data);
+            button.remove ();
+
+            // configure tooltips
+            mainTooltipConf ()
+        });
+    };
+
+    chart.listen ('pointClick', function (e) {
+        configDrillDown (e);
     });
 
     // initiate drawing the chart
     chart.draw ();
+
 });
